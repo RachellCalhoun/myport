ec92288487370fbd8ce00c168c3b0927964818be
rachell calhoun <chellybea@hotmail.com>
2015-07-19 22:11:21 +0900

    portfolio page reversed order by date, added skills

--- portfolio/views.py
+++ portfolio/views.py
@@ -3,6 +3,9 @@ from .models import Entry
 from django.shortcuts import get_object_or_404, render
 
 def portentry(request):
-	entries = Entry.objects.all()
+	unordered = Entry.objects.order_by('date')
+	entries = unordered.reverse()
 	context = {'entries':entries}
 	return render(request, 'portfolio/portfolio.html',context)
+	
+
