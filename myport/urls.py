131dd1477d0299a3d7fe46c145b3c12a0ef3450a
root <root@125.209.196.159>
2015-08-01 16:33:28 +0900

    fixed staticfiles issue- Jin

--- myport/urls.py
+++ myport/urls.py
@@ -18,6 +18,9 @@ from django.contrib import admin
 from django.conf import settings 
 from django.conf.urls.static import static 
 from django.views.generic import TemplateView
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib.staticfiles.urls import static 
+
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
@@ -27,3 +30,4 @@ urlpatterns = [
     url(r'^$',TemplateView.as_view(template_name='staticpages/home.html'),name='home'),
     url(r'^resume/',TemplateView.as_view(template_name='staticpages/resume.html'),name='resume'),
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
