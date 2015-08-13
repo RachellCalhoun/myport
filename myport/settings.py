896f4d9652906552d898b27b76d5f712bce45228
rachell calhoun <chellybea@hotmail.com>
2015-08-13 19:45:25 +0900

    fixed port footer and static path

--- myport/settings.py
+++ myport/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -103,9 +104,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-#STATIC_ROOT = os.path.join(BASE_DIR, '..','www','static')
-STATIC_ROOT = "/myport/staticpages/static"
+STATIC_ROOT = os.path.join(BASE_DIR, '..','www','static')
+#STATIC_ROOT = "/myport/staticpages/static"
 STATIC_URL = "/static/"
-MEDIA_ROOT =  "/myport/"
+#MEDIA_ROOT =  "/myport/"
 MEDIA_URL = '/media/'
-#STATIC_ROOT = os.path.join(BASE_DIR, '..','www','media')
+STATIC_ROOT = os.path.join(BASE_DIR, '..','www','media')
