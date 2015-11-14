a4c28e7c6d508cb66f32b9606d1f77b093732c9c
Rachell <chellybea@hotmail.com>
2015-11-14 14:43:53 +0900

    updated blog

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Entry
+from .models import Post, Comment
 
-admin.site.register(Entry)
+admin.site.register(Post)
+admin.site.register(Comment)
+# Register your models here.
