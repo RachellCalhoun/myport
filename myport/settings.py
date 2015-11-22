f28fb698d994f5ec08fd330eb51c2f82d1f82296
Rachell <chellybea@hotmail.com>
2015-11-22 21:07:50 +0900

    checking

--- myport/settings.py
+++ myport/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'b+qe9y(wdx&8fjzm^din1=+%3%1kgs*ralo(35st2n%0#w0iah'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
